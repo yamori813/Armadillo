c8ba82420083d80a02af7802af49db1b8d5ea6ee
himori <himori@example.com>
2010-09-11 02:45:09 +0000

    fixed copywrite
    git-svn-id: svn://svn.code.sf.net/p/ubsa-osx/svn/Armadillo@53 c0ceaddf-8823-4bce-a8de-5f6b89fa8458

--- main.m
+++ main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  Armadillo
 //
-//  Created by ? ?? on 10/09/05.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//  Created by H.M on 10/08/29.
+//  Copyright 2010 Hiroki Mori. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
