7ec38abafe7393b9667c91804b6cb9a4b1d9504b
himori <himori@example.com>
2010-09-13 22:56:14 +0000

    initial checkin
    git-svn-id: svn://svn.code.sf.net/p/ubsa-osx/svn/Armadillo@64 c0ceaddf-8823-4bce-a8de-5f6b89fa8458

---
+++ genir.c
@@ -0,0 +1,20 @@
+/*
+ *  genir.c
+ *  Armadillo
+ *
+ *  Created by H.M on 10/09/14.
+ *  Copyright 2010 Hiroki Mori. All rights reserved.
+ *
+ */
+
+#include "genir.h"
+
+int genir_crossam()
+{
+	return 0;
+}
+
+int genir_pcoprs1()
+{
+	return 0;
+}
---
+++ genir.h
@@ -0,0 +1,11 @@
+/*
+ *  genir.h
+ *  Armadillo
+ *
+ *  Created by H.M on 10/09/14.
+ *  Copyright 2010 Hiroki Mori. All rights reserved.
+ *
+ */
+
+int genir_crossam();
+int genir_pcoprs1();
