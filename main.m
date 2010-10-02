8fce9183ad9d5c141096ccb0d8e04c246f8710da
himori <himori@example.com>
2010-10-02 04:31:54 +0000

    add applescript command test code
    git-svn-id: svn://svn.code.sf.net/p/ubsa-osx/svn/Armadillo@151 c0ceaddf-8823-4bce-a8de-5f6b89fa8458

--- main.m
+++ main.m
@@ -12,3 +12,21 @@ int main(int argc, char *argv[])
 {
     return NSApplicationMain(argc,  (const char **) argv);
 }
+
+@interface ArmadilloApp : NSApplication
+@end
+@implementation ArmadilloApp
+-(void)ArmadilloTest:(NSScriptCommand*)command {
+	printf("MORI MORI AppleScript");
+	/*
+	NSDictionary*	theArgs = [command evaluatedArguments];
+	NSString*		encodeString = [theArgs objectForKey:@"textWith"];
+	NSData *sjisData = [ encodeString dataUsingEncoding: 
+						NSShiftJISStringEncoding ];
+	
+	[[NSNotificationCenter defaultCenter] postNotificationName: 
+	 @"ServiceNotificaiton" object: sjisData];
+*/
+}
+
+@end
